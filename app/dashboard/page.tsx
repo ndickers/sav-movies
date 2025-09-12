08d1523c6d312646bf155a70716b26b869d372ee
Ndickers <bryondickers@gmail.com>
2025-09-13 01:59:03 +0300

    fix(UI) login page by increasing font size

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,11 +1,27 @@
-import React from "react";
+import MovieGrid from "../components/MovieGrid";
 import Navbar from "../components/Navbar";
 
-export default function page() {
+async function getMovies() {
+  const res = await fetch(
+    `${process.env.NEXT_PUBLIC_BASE_URL}/api/movies?api_key=${process.env.TMDB_API_KEY}`,
+    {
+      cache: "no-store",
+    }
+  );
+  return res.json();
+}
+
+export default async function Home() {
+  const movies = await getMovies();
+  console.log({ movies });
+
   return (
-    <div>
-      <Navbar />
-      dashboard
-    </div>
+    <main className="max-w-6xl mx-auto p-6">
+      <div>
+        <h1 className="text-3xl font-bold mb-6">🎬 Recommended Movies</h1>
+        <Navbar />
+      </div>
+      <MovieGrid movies={movies} loading={!movies} />
+    </main>
   );
 }
--- app/page.tsx
+++ app/page.tsx
@@ -6,14 +6,16 @@ export default function Home() {
   return (
     <div className="flex justify-center items-center">
       <div className="py-24">
-        <h1 className="font-black">Movies Recommendation</h1>
+        <h1 className="font-black text-xl md:text-2xl ">
+          Movies Recommendation
+        </h1>
 
-        <p className="text-center mt-6">Sign in</p>
+        <p className="text-center text-base mt-6">Sign in</p>
         <button
           onClick={() => {
             signIn("google", { callbackUrl: "/dashboard" });
           }}
-          className="flex items-center justify-center text-xs gap-1.5 border border-[#D9D9D9))]  mt-6 lg:mt-4 w-full rounded-md py-1.5 lg:py-1"
+          className="flex items-center justify-center text-sm md:text-base gap-1.5 border border-[#D9D9D9))]  mt-6 lg:mt-4 w-full rounded-md py-1.5 lg:py-1"
         >
           <Image
             src={"/icons-google.svg"}
