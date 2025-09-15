709d2fa4132f72f99adc381a0e29e486ba384a10
Ndickers <bryondickers@gmail.com>
2025-09-15 17:31:01 +0300

    Fix: Linting error when deploying

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,10 @@
+import lineClamp from "@tailwindcss/line-clamp";
+
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ["./app/**/*.{js,ts,jsx,tsx}", "./components/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {},
   },
-  plugins: [require("@tailwindcss/line-clamp")],
+  plugins: [lineClamp],
 };
